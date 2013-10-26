812c5cd5f9d64f6543f7810c6cef1f302fdef082
Joshua Guy <jglenes@gmail.com>
2013-10-26 11:42:18 -0400

    First custom .bash-it theme

---
+++ aliases/available/vlc.aliases.bash
@@ -0,0 +1,4 @@
+cite 'about-alias'
+about-alias 'VLC aliases'
+
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC -I rc'
---
+++ lib/paths.bash
@@ -0,0 +1,9 @@
+# Setting our $PATH
+
+# Use homebrewed paths
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH # Use homebrewed paths by default
+export PATH=/usr/local:$PATH
+
+# Other PATHs
+export PATH=/usr/local/share/npm/bin:$PATH # Node.js
+export PATH=$HOME/.cabal/bin:$PATH # Haskell?
